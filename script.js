f311b329c5a01a541bf85a7dea516ecf222938a7
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-09 15:34:12 -0400

    first page

---
+++ script.js
@@ -0,0 +1,53 @@
+var elements = document.getElementsByClassName("hideable");
+var vis = getVisible(elements) + 1;
+function toggleSlide(direction)
+{
+    // gets all the "slides" in our slideshow
+    var elements = document.getElementsByClassName("hideable");
+
+    // Find the LI that's currently displayed
+    var visibleID = getVisible(elements);
+
+    // hide the currently visible LI
+    elements[visibleID].style.display = "none";
+
+    // get the previous slide
+    if(!direction)
+    {
+        var makeVisible = prev(visibleID, elements.length); 
+    }
+
+    // get the next slide
+    else
+    {
+        var makeVisible = next(visibleID, elements.length); 
+    }
+
+    // show the previous or next slide
+    elements[makeVisible].style.display = "block"; 
+    //var sn = document.getElementById("slideNumber");
+    //sn.innerHTML = (makeVisible + 1);
+}
+function getVisible(elements) {
+    var visibleID = -1;
+    for(var i = 0; i < elements.length; i++) {
+        if(elements[i].style.display == "block") {
+            visibleID = i;
+        }
+    }
+    return visibleID;
+}
+ 
+function prev(num, arrayLength) {
+    if(num == 0) return arrayLength-1;
+    else return num-1;
+}
+ 
+function next(num, arrayLength) {
+    if(num == arrayLength-1) return 0;
+    else return num+1;
+}
+var myVar = setInterval(timermove, 4000);
+function timermove(){
+    toggleSlide(true);
+}
