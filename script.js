89d00812a740b665aa6f9c5221e84084a07c7f94
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-10 12:09:03 -0400

    now have 5 posters

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var images=null;
+var index = 0;
 function onSearch() 
 {
     var title = $('input[name=s]').val();
@@ -27,76 +29,14 @@ function onSearch()
         dataType: "json",   
         success: function(response) 
         {    
+          images=response;
           console.log(response);
           $("#poster").html('<img src = "' + response.Search[0].Poster + '"/>');
         }
     });
   }
-
-/*
-var elements = document.getElementsByClassName("hideable");
-var vis = getVisible(elements) + 1;
-function toggleSlide(direction)
->>>>>>> b728274d995a32aee5b7a7cd2245e56d66376199
-{
-   url: "http://www.omdbapi.com/?",   data:
-    { 
-      s: $('input[name=t]').val()
-    }
-   ,   dataType: "json",   
-   success: function(response)
-    {   console.log(response);
-      if(response.Response=="False")
-      {
-        $("#return").html('<h1>'+" no movie with that name found"+'</h1>');
-        $("#plot").html("");
-        $("#poster").html("");
-      }
-      else
-      {
-        $("#return").html('<h1> ' + response.Title + ' </h1>');
-        $("#plot").html(response.Plot);
-        $("#poster").html('<img src = " ' + response.Poster + ' "/>');
-      }
-    } 
-});
-
-<<<<<<< HEAD
-return false;
+function next(){
+  index= (index+1)%5;
+  $("#poster").html('<img src = "' + images.Search[index].Poster + '"/>');
 }
-=======
-    // get the next slide
-    else
-    {
-        var makeVisible = next(visibleID, elements.length); 
-    }
-
-    // show the previous or next slide
-    elements[makeVisible].style.display = "block"; 
-    //var sn = document.getElementById("slideNumber");
-    //sn.innerHTML = (makeVisible + 1);
-}
-function getVisible(elements) {
-    var visibleID = -1;
-    for(var i = 0; i < elements.length; i++) {
-        if(elements[i].style.display == "block") {
-            visibleID = i;
-        }
-    }
-    return visibleID;
-}
- 
-function prev(num, arrayLength) {
-    if(num == 0) return arrayLength-1;
-    else return num-1;
-}
- 
-function next(num, arrayLength) {
-    if(num == arrayLength-1) return 0;
-    else return num+1;
-}
-var myVar = setInterval(timermove, 4000);
-function timermove(){
-    toggleSlide(true);
-}*/
 
