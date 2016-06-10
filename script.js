fc6205c65c648667ff00574fb3c244ef4b6caad5
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-10 10:31:45 -0400

    color change

--- script.js
+++ script.js
@@ -1,53 +1,27 @@
-var elements = document.getElementsByClassName("hideable");
-var vis = getVisible(elements) + 1;
-function toggleSlide(direction)
+function search(){
+var test  = $.ajax(
 {
-    // gets all the "slides" in our slideshow
-    var elements = document.getElementsByClassName("hideable");
-
-    // Find the LI that's currently displayed
-    var visibleID = getVisible(elements);
-
-    // hide the currently visible LI
-    elements[visibleID].style.display = "none";
-
-    // get the previous slide
-    if(!direction)
-    {
-        var makeVisible = prev(visibleID, elements.length); 
+   url: "http://www.omdbapi.com/?",   data:
+    { 
+      s: $('input[name=t]').val()
     }
+   ,   dataType: "json",   
+   success: function(response)
+    {   console.log(response);
+      if(response.Response=="False")
+      {
+        $("#return").html('<h1>'+" no movie with that name found"+'</h1>');
+        $("#plot").html("");
+        $("#poster").html("");
+      }
+      else
+      {
+        $("#return").html('<h1> ' + response.Title + ' </h1>');
+        $("#plot").html(response.Plot);
+        $("#poster").html('<img src = " ' + response.Poster + ' "/>');
+      }
+    } 
+});
 
-    // get the next slide
-    else
-    {
-        var makeVisible = next(visibleID, elements.length); 
-    }
-
-    // show the previous or next slide
-    elements[makeVisible].style.display = "block"; 
-    //var sn = document.getElementById("slideNumber");
-    //sn.innerHTML = (makeVisible + 1);
-}
-function getVisible(elements) {
-    var visibleID = -1;
-    for(var i = 0; i < elements.length; i++) {
-        if(elements[i].style.display == "block") {
-            visibleID = i;
-        }
-    }
-    return visibleID;
-}
- 
-function prev(num, arrayLength) {
-    if(num == 0) return arrayLength-1;
-    else return num-1;
-}
- 
-function next(num, arrayLength) {
-    if(num == arrayLength-1) return 0;
-    else return num+1;
-}
-var myVar = setInterval(timermove, 4000);
-function timermove(){
-    toggleSlide(true);
+return false;
 }
